30c23c73a14420228f2accee17fa10cd08d7ddc5
seonyo <jsy6535@naver.com>
2023-12-16 17:54:48 +0900

    쿠키 암호화 하기

--- app_cookie.js
+++ app_cookie.js
@@ -3,16 +3,17 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const port = 3000
 
-app.use(cookieParser());
+app.use(cookieParser('234qsdf#dDbd2$@!a'));
 
 app.get('/count', function(req, res){
-    if(req.cookies.count){
-        var count = parseInt(req.cookies.count);
+    if(req.signedCookies.count){
+        var count = parseInt(req.signedCookies.count);
     } else {
         var count = 0;
     }
     count = count+1;
-    res.cookie('count', count);     //cookieParser 안의 cookie라는 메소드를 사용하여 cookie 넘겨주기
+    res.cookie('count', count, {signed:true});     //cookieParser 안의 cookie라는 메소드를 사용하여 cookie 넘겨주기
+    console.log(count)
     res.send('count : ' + count);   // 전송하는 쿠키의 값
 });
 
